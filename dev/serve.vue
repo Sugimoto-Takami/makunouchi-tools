722c30847f1de49f8bc59b9f8982b4c0cc96e336
Sugimoto-Takami <minminzemi.gabugabu@gmail.com>
2024-08-14 20:26:43 +0900

    first commit

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <hello-world />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { MakunouchiToolsSample } from '@/entry.esm';
+
+export default defineComponent({
+  name: 'ServeDev',
+  // components: {
+  //  MakunouchiToolsSample,
+  // }
+});
+</script>
---
+++ src/lib-components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>
+        Hello World!
+    </h1>
+</template>
+
+<script setup lang="ts">
+// script setup の場合、コンポーネント名はファイル名から自動的に推論される
+// defineComponent({
+//     name: 'HelloWorld'
+// })
+</script>
